5584bd2c8c9f4343f9ceebff55b362155b3e1e42
c3730915 <730915.com>
2021-10-03 15:31:23 +0800

    Initial commit

---
+++ order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -0,0 +1,26 @@
+package cn.itcast.order;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@MapperScan("cn.itcast.order.mapper")
+@SpringBootApplication
+public class OrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+
+    /**
+     * 创建restTemplate 并且注入spring
+     * @return
+     */
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+}
